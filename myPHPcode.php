f1b44ca355df31e213ff584aa94236b8fd080745
Volsky13 <106740254+Volsky13@users.noreply.github.com>
2022-06-02 17:36:43 +0700

    Create myPHPcode.php

---
+++ myPHPcode.php
@@ -0,0 +1,16 @@
+<?php
+$name = "my name is valerie Attila alfath"
+$age = "and my age is 18"
+$address = "i live in Indonesia, in medan city"
+
+echo $name + "<br>" + $age + "<br>" + $address;
+
+
+
+
+
+
+
+
+
+?>
